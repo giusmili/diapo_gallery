422e901eb72963df7eb313c1e05a2fb1cb0e710b
giusmili <giusmili67@gmail.com>
2021-03-30 15:03:15 +0200

    modification content

--- js/flash.js
+++ js/flash.js
@@ -1,13 +1,40 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log(200);
 
-    let parents, el;
+    let parents, el, texte, content, dayNight, superparent, header, loop, bool;
+    texte = document.querySelector(".material-icons");
+    content = document.querySelector("#content > p");
     parents = document.querySelector(".wrapper");
     el = document.createElement("div");
+    dayNight = document.querySelectorAll("#header .fas")[1];
+    loop = document.querySelectorAll("#header .fas")[0];
+    superparent = document.querySelector("body");
+    header = document.querySelector("#header h1");
     parents.appendChild(el);
     console.log(el);
     el.setAttribute("class", "contentInfo");
     el.setAttribute("role", "region");
     el.setAttribute("aria-labelledby", "barry");
     el.innerHTML += '<p id="barry">Flash : Barry Allen </p>';
+
+    /*event*/
+    texte.addEventListener("click", e => {
+        console.log(e.target);
+        console.log(content);
+        content.classList.toggle("align-text");
+        //texte.innerText = "format_align_left"
+        texte.innerText == "format_align_center" ? texte.innerText = "format_align_left" : texte.innerText = "format_align_center";
+    });
+
+    //jour et nuit
+    dayNight.addEventListener("click", () => {
+        console.log("ok");
+        superparent.classList.toggle("night_day");
+        dayNight.classList.toggle("night_day_text");
+        header.classList.toggle("night_day_text");
+    });
+    loop.addEventListener("click", e => {
+        console.log(e.target);
+        superparent.classList.toggle("zoom");
+    });
 });
